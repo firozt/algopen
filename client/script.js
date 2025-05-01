77ee9de5c232267c1adc06e2d0039437cfb5d615
firozt <firozt03@gmail.com>
2025-05-01 04:08:16 +0100

    added zoom

--- client/script.js
+++ client/script.js
@@ -9,6 +9,34 @@ const stage = new Konva.Stage({
     draggable: true
 });
 
+const scaleBy = 1.05;
+
+stage.on('wheel', (e) => {
+    e.evt.preventDefault();
+
+    const oldScale = stage.scaleX();
+
+    const pointer = stage.getPointerPosition();
+    const mousePointTo = {
+        x: (pointer.x - stage.x()) / oldScale,
+        y: (pointer.y - stage.y()) / oldScale,
+    };
+
+    const direction = e.evt.deltaY > 0 ? 1 : -1;
+    const newScale = direction > 0 ? oldScale / scaleBy : oldScale * scaleBy;
+
+    stage.scale({ x: newScale, y: newScale });
+
+    const newPos = {
+        x: pointer.x - mousePointTo.x * newScale,
+        y: pointer.y - mousePointTo.y * newScale,
+    };
+
+    stage.position(newPos);
+    stage.batchDraw();
+});
+
+
 const layer = new Konva.Layer();
 stage.add(layer);
 
