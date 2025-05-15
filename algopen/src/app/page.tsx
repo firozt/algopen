6d4eebb70968d9d0a0e859c011a62731e8b5c4b7
firozt <firozt03@gmail.com>
2025-05-15 18:19:30 +0100

    added auto redirect for prod

--- algopen/src/app/page.tsx
+++ algopen/src/app/page.tsx
@@ -1,5 +1,11 @@
+'use client'
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    redirect('/visualiser');
+  },[])
   return (
     <div>
       <h1>index?</h1>
