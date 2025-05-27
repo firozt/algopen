7884676ef26a567afb9a9e40a0374f200fff2e11
firozt <firozt03@gmail.com>
2025-05-27 14:29:17 +0100

    build

--- algopen/src/app/pathfinding-visualiser/page.tsx
+++ algopen/src/app/pathfinding-visualiser/page.tsx
@@ -2,16 +2,14 @@
 import React, { useEffect } from 'react'
 import './index.css'
 import { redirect } from 'next/navigation'
-import ToolBar from '../components/ToolBar/ToolBar'
 
 const Page = () => {
 
     useEffect(() => {
-        // redirect('/not-found')
+        redirect('/not-found')
     },[])
   return (
     <div>
-        <ToolBar toggle={() => 1} title='this is the title' />
     </div>
   )
 }
